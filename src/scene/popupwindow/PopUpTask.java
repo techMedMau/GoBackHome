9d535f97aeb63988636a81589d37a404f78fc9b5
Maureen Chang <>
2021-04-14 15:39:15 +0800

    忘記commit

---
+++ src/scene/popupwindow/PopUpTask.java
@@ -0,0 +1,69 @@
+package scene.popupwindow;
+
+import controllers.ImageController;
+import gameobj.button.Button;
+import utils.CommandSolver;
+
+import java.awt.*;
+
+public class PopUpTask extends PopUpWindows{
+    public Button close;
+    public Image img;
+
+
+
+    public PopUpTask() {
+        super(600, 500);
+    }
+
+    @Override
+    public void sceneBegin() {
+        this.img = ImageController.getInstance().tryGet("/taskBackground.png");
+        close=new gameobj.button.Button(110,50,48,48, ImageController.getInstance().tryGet("/button/close.png"));
+        show();
+    }
+
+    @Override
+    public void sceneEnd() {
+
+    }
+
+    @Override
+    public CommandSolver.MouseListener mouseListener() {
+        return (e, state, trigTime) -> {
+            switch (state){
+                case CLICKED:
+                    if(!getShow()){
+                        show();
+                        break;
+                    }
+                    if (close.state(e.getPoint())){
+                        disShow();
+                        break;
+                    }
+
+                    break;
+            }
+        };
+    }
+
+    @Override
+    public CommandSolver.KeyListener keyListener() {
+        return null;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(img,158,50,null);
+        close.paint(g);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+//    public int left(){
+//        return img.
+//    }
+}
